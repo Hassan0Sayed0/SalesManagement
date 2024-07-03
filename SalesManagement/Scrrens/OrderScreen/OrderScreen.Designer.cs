b3634c281fe353a3e89fd0854755b92665f11dae
hassan0sayed0 <hassansayed3999@gmail.com>
2024-07-03 03:23:16 +0300

    make orferScreen first screen

--- SalesManagement/Scrrens/OrderScreen/OrderScreen.Designer.cs
+++ SalesManagement/Scrrens/OrderScreen/OrderScreen.Designer.cs
@@ -28,11 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnPay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPay
+            // 
+            this.btnPay.Location = new System.Drawing.Point(277, 301);
+            this.btnPay.Name = "btnPay";
+            this.btnPay.Size = new System.Drawing.Size(75, 23);
+            this.btnPay.TabIndex = 0;
+            this.btnPay.Text = "Pay";
+            this.btnPay.UseVisualStyleBackColor = true;
+            this.btnPay.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // OrderScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 347);
+            this.Controls.Add(this.btnPay);
+            this.Name = "OrderScreen";
             this.Text = "OrderScreen";
+            this.Load += new System.EventHandler(this.OrderScreen_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnPay;
     }
 }
--- SalesManagement/Scrrens/OrderScreen/OrderScreen.cs
+++ SalesManagement/Scrrens/OrderScreen/OrderScreen.cs
@@ -16,5 +16,15 @@ namespace SalesManagement.Scrrens.OrderScreen
         {
             InitializeComponent();
         }
+
+        private void OrderScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
